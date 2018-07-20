bbe731f4eaf1c3bee9b97829103f37e942f02a56
Sam Collard <swift@swifteagle.co.uk>
2018-07-20 13:41:37 +0100

    Delete all

--- help_command.js
+++
@@ -1,6 +0,0 @@
-function do_help() {
-    console.log("Supported commands are List, Import [filename] for CSV, XML, JSON");
-}
-do_help.requiresAccounts = false;
-
-module.exports = {do_help}
--- index.js
+++
@@ -1,4 +0,0 @@
-const cmd_loop = require('./command_loop');
-
-console.log("Press Ctrl-C to quit");
-cmd_loop.begin();
