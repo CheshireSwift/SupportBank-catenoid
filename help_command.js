80334810ee175267d6ffbb888b095fe4d5b4e254
Sam Collard <swift@swifteagle.co.uk>
2018-07-20 13:44:08 +0100

    Revert "Delete all"
    This reverts commit bbe731f4eaf1c3bee9b97829103f37e942f02a56.

---
+++ help_command.js
@@ -0,0 +1,6 @@
+function do_help() {
+    console.log("Supported commands are List, Import [filename] for CSV, XML, JSON");
+}
+do_help.requiresAccounts = false;
+
+module.exports = {do_help}
---
+++ index.js
@@ -0,0 +1,4 @@
+const cmd_loop = require('./command_loop');
+
+console.log("Press Ctrl-C to quit");
+cmd_loop.begin();
